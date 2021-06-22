927c3c4df840e1817d95ade898f3cb7ea2d7e0e3
Brian Parker <brian.parker@hotmail.com.au>
2021-06-22 21:24:21 +1000

    Edit Modal Example

--- HowToModal/Models/SomeDataModel.cs
+++ HowToModal/Models/SomeDataModel.cs
@@ -3,11 +3,17 @@
 // Free to use just pay your knowledge forward
 // -------------------------------------------
 
+using System;
+
 namespace HowToModal.Models
 {
     public class SomeDataModel
     {
-
+        public int Id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
     }
+
+
 }
---
+++ HowToModal/Views/Pages/ModalListExample.razor.cs
@@ -0,0 +1,57 @@
+// -------------------------------------------
+// Copyright (c) 2021, Brian Parker
+// Free to use just pay your knowledge forward
+// -------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using HowToModal.Models;
+using HowToModal.Views.Components;
+
+namespace HowToModal.Views.Pages
+{
+    public partial class ModalListExample
+    {
+        private TemplateModal<SomeDataModel> modal;
+        private SomeDataModel currentModel;
+        private IEnumerable<SomeDataModel> someData;
+
+        protected override void OnInitialized() 
+        {
+            someData = Enumerable.Range(1, 20).Select(i => new SomeDataModel
+            {
+                Id = i,
+                Name = $"Name {i}",
+                DateOfBirth = DateTimeOffset.Now - TimeSpan.FromDays(i * 25)
+            }).ToList();
+        }
+
+        private void OpenModal(SomeDataModel data)
+        {
+            currentModel = data;
+            var temp = new SomeDataModel();
+            CopyModel(data, temp);
+            modal.ShowModal(temp);
+        }
+
+
+        private void ModalClosed(ModalResult<SomeDataModel> modalResult)
+        {
+            if(modalResult.CloseState == ModalCloseState.Ok)
+            {
+                CopyModel(modalResult.Value, currentModel);
+                StateHasChanged();
+            }
+            
+        }
+
+        private void CopyModel(SomeDataModel source, SomeDataModel dest)
+        {
+            dest.Id = source.Id;
+            dest.Name = source.Name;
+            dest.DateOfBirth = source.DateOfBirth;
+            dest.Email = source.Email;
+        }
+    }
+}
